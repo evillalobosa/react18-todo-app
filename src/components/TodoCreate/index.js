1a6d83827a0e78a25585a5c9ea658a1960d46321
Enrique Villalobos Andrade <enrique.lnx@gmail.com>
2023-05-05 19:38:53 -0400

    Regex to match priority and tags

--- src/components/TodoCreate/index.js
+++ src/components/TodoCreate/index.js
@@ -4,14 +4,42 @@ import "./index.css";
 
 function TodoCreate() {
   const { searchValue, setSearchValue } = React.useContext(TodoContext);
+  const { priority, setPriority } = React.useState("");
+  const { tag, setTag } = React.useState("");
+
+  const onSubmit = (event) => {
+    // Prevent page reload
+    event.preventDefault();
+  };
 
   const onTyping = (event) => {
-    setSearchValue(event.target.value);
+    const userInput = event.target.value;
+    setSearchValue(userInput);
+
+    // MATCH PRIORITY (1-4)
+    const prioritiesRegex = /(^|\s)p[1-4](\s|$)/;
+    const matchPriority = userInput.match(prioritiesRegex);
+    const inputHTML = document.querySelector("input");
+
+    if (matchPriority) {
+      console.log("Aplicando prioridad: ", matchPriority[0]);
+      inputHTML.style.backgroundColor = "var(--color-priority1)";
+    } else {
+      inputHTML.style.backgroundColor = "var(--color-accent)";
+    }
+
+    // MATCH TAG
+    const tagsRegex = /(^|\s)@[a-zA-Z]{1,10}(\s|$)/;
+    const matchTag = userInput.match(tagsRegex);
+
+    if (matchTag) {
+      console.log("Aplicando tag: ", matchTag[0]);
+    }
   };
 
   // TODO: add a button to clear the input 🗑️
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <input
         type="text"
         placeholder="Create new tasks here!"
