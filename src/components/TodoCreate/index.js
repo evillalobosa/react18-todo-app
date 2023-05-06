880d41709cfc46fa409457700e91eb2c3f8e8d8f
Enrique Villalobos Andrade <enrique.lnx@gmail.com>
2023-05-05 20:32:02 -0400

    Showing the priority and tag of the task above the text input field

--- src/components/TodoCreate/index.js
+++ src/components/TodoCreate/index.js
@@ -4,8 +4,8 @@ import "./index.css";
 
 function TodoCreate() {
   const { searchValue, setSearchValue } = React.useContext(TodoContext);
-  const { priority, setPriority } = React.useState("");
-  const { tag, setTag } = React.useState("");
+  const [priority, setPriority] = React.useState("");
+  const [tag, setTag] = React.useState("");
 
   const onSubmit = (event) => {
     // Prevent page reload
@@ -16,37 +16,45 @@ function TodoCreate() {
     const userInput = event.target.value;
     setSearchValue(userInput);
 
-    // MATCH PRIORITY (1-4)
+    // MATCH PRIORITY
+    // Detects if the user input contains the priority of the task
+    // The priority is set as: p1, p2, p3 or p4.
     const prioritiesRegex = /(^|\s)p[1-4](\s|$)/;
     const matchPriority = userInput.match(prioritiesRegex);
-    const inputHTML = document.querySelector("input");
 
     if (matchPriority) {
       console.log("Aplicando prioridad: ", matchPriority[0]);
-      inputHTML.style.backgroundColor = "var(--color-priority1)";
-    } else {
-      inputHTML.style.backgroundColor = "var(--color-accent)";
+      setPriority(matchPriority[0]);
     }
 
     // MATCH TAG
+    // Detects if the user input contains the tag of the task
+    // The tag is a word no longer than 10 characters
     const tagsRegex = /(^|\s)@[a-zA-Z]{1,10}(\s|$)/;
     const matchTag = userInput.match(tagsRegex);
 
     if (matchTag) {
       console.log("Aplicando tag: ", matchTag[0]);
+      setTag(matchTag[0]);
     }
   };
 
   // TODO: add a button to clear the input 🗑️
   return (
-    <form onSubmit={onSubmit}>
-      <input
-        type="text"
-        placeholder="Create new tasks here!"
-        value={searchValue}
-        onChange={onTyping}
-      />
-    </form>
+    <div className="taskInput">
+      <div className="inputProps">
+        {priority && <div className="priority">{priority}</div>}
+        {tag && <div className="tag">{tag}</div>}
+      </div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Create new tasks here!"
+          value={searchValue}
+          onChange={onTyping}
+        />
+      </form>
+    </div>
   );
 }
 
